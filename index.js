d200cab84f3de54c82efb32a9a59c5514f3044c6
nguyenDoton <76211824+nguyenDoton@users.noreply.github.com>
2021-06-19 11:37:21 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let in4 =[{
+   Email:"gobrbrbrbr@gmail.com",
+   Phone:"0123456789",
+   Password:"qwertyuiop",
+   Username:"None",
+},
+{
+    Email:"brbrbrbr@gmail,com",
+    Phone:"0123467890",
+    Password:"asdfghjkl",
+    Username:"StillNone",
+},
+{
+    Email:"khongcoemail@gmail.com",
+    Phone:"0198765432",
+    Password:"jiddjdjdj",
+    Username:"StillNone1",
+},
+]
+localStorage.setItem("Infor",JSON.stringify(in4))
